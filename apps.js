fd4558b19984d3702ad42da70604e4b42122a50e
gabybaty <batistuta9.gb@gmail.com>
2021-05-12 16:00:07 -0300

    AGREGAR SWITCH

--- apps.js
+++ apps.js
@@ -1,1 +1,14 @@
 const {sumar,restar,multiplicar,dividir} = require('./calculadora')
+
+switch (ProcessingInstruction.argv[2]){
+    case "sumar" :
+        sumar(process.argv[3],process.argv[2])
+        break;
+        case"restar" :
+        restar(porcess.argv[3],porcess.argv[2])
+        break;
+
+        default:
+            break;
+
+}
